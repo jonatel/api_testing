eb5dda9de481e9f18f88b885aece5a258fb2e61f
José Oñate López <jose.onate.lopez@gmail.com>
2022-02-02 19:10:18 -0500

    simple flask app

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,20 @@
-print("Hello World!!!")
+"""
+hello_flask: First Python-Flask webapp
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+
+
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world!'
+
+
+  if __name__ == '__main__':
+    print("Hello World! Built with a Docker file.")
+    # Launch built-in web server and run this Flask webapp
+    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
