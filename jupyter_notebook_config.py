1a8c8aa5b8412afe3adc88dc21f23042240d42a5
José Oñate López <jose.onate.lopez@gmail.com>
2022-02-03 03:46:00 -0500

    Create jupyter_notebook_config.py

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,12 @@
+
+import notebook.auth
+
+
+c = get_config()  # noqa: F821
+c.NotebookApp.ip = "0.0.0.0"
+c.NotebookApp.port = 8080
+c.NotebookApp.open_browser = False
+c.NotebookApp.quit_button = False
+
+# password
+c.NotebookApp.password = notebook.auth.passwd("genesys")
